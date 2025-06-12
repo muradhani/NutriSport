ad969bbf3bacb2846efad3a449339c06100181ed
muradhani <mouradhany574@gmail.com>
2025-06-12 22:17:15 +0300

    add the splash screen for ios and android

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -34,6 +34,7 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.androidx.core.splashscreen)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
