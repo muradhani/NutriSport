3e51ced91f2a7534d205a99eb661b95c586ec4d5
muradhani <mouradhany574@gmail.com>
2025-06-13 17:23:10 +0300

    create alpha object

---
+++ shared/src/commonMain/kotlin/com/example/shared/Alpha.kt
@@ -0,0 +1,11 @@
+package com.example.shared
+
+import androidx.compose.ui.unit.sp
+
+object Alpha {
+    const val FULL = 1f
+    const val HALF = .5f
+    const val DISABLED = 0.38F
+    const val TEN_PERCENT = 0.1F
+    const val ZERO = 0.0F
+}
