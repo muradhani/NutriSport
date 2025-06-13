7fc16b4190ae218a1d46e028196aa509c9f0feba
muradhani <mouradhany574@gmail.com>
2025-06-13 17:14:56 +0300

    create colors and resource object

---
+++ shared/src/commonMain/kotlin/com/example/shared/Colors.kt
@@ -0,0 +1,30 @@
+package com.example.shared
+
+import androidx.compose.ui.graphics.Color
+
+
+val GrayLighter = Color(0xFFFAFAfA)
+val Gray = Color(0xFFF1F1F1)
+val GrayDarker = Color(0xFFEBEBEB)
+
+val Yellowish = Color(0xFFEEFF00)
+val Orange = Color(0xFFF24C00)
+val White = Color(0xFFFFFFF)
+val Black = Color(0xFF000000)
+
+val Surface = White
+val SurfaceLighter = GrayLighter
+val SurfaceDarker = Gray
+
+val TextPrimary = Black
+val TextSecondary = GrayLighter
+val TextWhite = White
+val textBrand = Yellowish
+
+val ButtonPrimary = Yellowish
+val ButtonSecondary = GrayDarker
+val ButtonDisabled = GrayDarker
+
+val IconPrimary = Black
+val IconSecondary = Orange
+val IconWhite = White
---
+++ shared/src/commonMain/kotlin/com/example/shared/Resources.kt
@@ -0,0 +1,38 @@
+package com.example.shared
+
+import nutrisport.shared.generated.*
+import nutrisport.shared.generated.resources.Res
+import nutrisport.shared.generated.resources.*
+
+
+object Resources {
+    object Icon{
+        val plus = Res.drawable.plus
+        val minus = Res.drawable.minus
+        val singIn = Res.drawable.log_in
+        val singOut = Res.drawable.log_out
+        val unlock = Res.drawable.unlock
+        val search = Res.drawable.search
+        val person = Res.drawable.user
+        val checkMark = Res.drawable.checkmark_image
+        val edit = Res.drawable.edit_3
+        val menu = Res.drawable.menu
+        val backArrow = Res.drawable.back_arrow
+        val rightArrow = Res.drawable.right_arrow
+        val home = Res.drawable.home
+        val shoppingCart = Res.drawable.shopping_cart
+        val categories = Res.drawable.grid
+        val dollar = Res.drawable.dollar
+        val mapPin = Res.drawable.map_pin
+        val close = Res.drawable.close
+        val book = Res.drawable.book
+        val verticalMenu = Res.drawable.vertical_menu
+        val delete = Res.drawable.delete
+    }
+    object Image {
+        val shoppingCart = Res.drawable.shopping_cart_image
+        val checkMark = Res.drawable.checkmark_image
+        val cat = Res.drawable.cat
+
+    }
+}
