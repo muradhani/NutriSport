7aecb32d2684afd63dee6237cd6c3191f5c59419
muradhani <mouradhany574@gmail.com>
2025-07-05 16:31:54 +0300

    Integrate Google Sign In with Firebase

--- feature/auth/src/commonMain/kotlin/com/nutrisport/auth/screens/AuthScreen.kt
+++ feature/auth/src/commonMain/kotlin/com/nutrisport/auth/screens/AuthScreen.kt
@@ -8,26 +8,32 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.currentCompositionLocalContext
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.shared.BebasNeueFontFamily
 import com.example.shared.*
+import com.mmk.kmpauth.firebase.google.GoogleButtonUiContainerFirebase
 
 
 import com.nutrisport.auth.component.GoogleButton
 
 @Composable
 fun AuthScreen(){
+    var loadingState by remember { mutableStateOf(false) }
     Scaffold (
         modifier = Modifier.fillMaxSize(),
         containerColor = Surface
     ){ padding ->
+        val context = rememberPlatformContext()
         Column (
             modifier = Modifier.padding(
                 top = padding.calculateTopPadding(),
@@ -56,10 +62,34 @@ fun AuthScreen(){
                     color = Black
                 )
             }
-            GoogleButton(
-                loading = false,
-                onClick = { }
-            )
+            GoogleButtonUiContainerFirebase(
+                linkAccount = false,
+                onResult = { result ->
+                    result.onSuccess { user ->
+                        loadingState = false
+
+                    }.onFailure { error ->
+                        var displayMessage = "An unknown error occurred."
+                        if (error.message?.contains("A network error") == true){
+                            displayMessage = "A network error occurred. Please check your connection."
+                        }else if (error.message?.contains("Idtoken is null") == true){
+                            displayMessage = "Sign in failed. Please try again."
+                        }else{
+                            displayMessage = "Sign in failed: ${error.message}"
+                        }
+                        showToast(displayMessage,context)
+                        loadingState = false
+                    }
+                }
+            ) {
+                GoogleButton(
+                    loading = false,
+                    onClick = {
+                        loadingState = true
+                        this@GoogleButtonUiContainerFirebase.onClick()
+                    }
+                )
+            }
         }
     }
 }
